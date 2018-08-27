a31d533fb508cea6444e1a49fa5f60814827b5d4
Estelle Tidey <ektidey@gmail.com>
2018-08-27 17:47:27 +0100

    Create home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Triad Weyrs</title>
+
+        <meta name="author" content="Triad Weyrs">
+        <meta name="description" 
+            content="A post-AIVAS Tenth Pass Pern&copy; play-by-email club, with three
+                    Weyrs, six Holds, and twelve Halls to play dragonriders, holders, or crafters.">
+        <meta name="keywords" 
+            content="pern pbem, Dragonriders of Pern, Pern, Anne McCaffrey, McCaffrey, dragon,
+                    dragons, RPG, role play, game, PBEM, sci-fi, science-fiction, fantasy, role-play,
+                    dragonsfall, weyr, weyrs, fanfic, fanfiction, roleplaying game, Dragonsdawn, Moreta,
+                    Nerilka, Dragonquest, Dragonflight, Dragonseye, Red Star Rising, fan-fic, dragonrider,
+                    Hatchings, Healercraft, Harpercraft, Tannercraft, Smithcraft, AIVAS"> 
+
+        <link rel="icon" href="/favicon.ico">
+        <link href="/css/app.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <h1>Welcome to Triad Weyrs!</h1>
+    </body>
+</html>
